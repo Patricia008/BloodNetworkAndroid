950b7c94894243f6886aad2e1707b41d6442fd63
Patricia008 <petrut.patricia@gmail.com>
2017-01-08 14:46:08 +0200

    Sign in service

--- app/src/main/java/com/upt/cti/bloodnetwork/SignIn.java
+++ app/src/main/java/com/upt/cti/bloodnetwork/SignIn.java
@@ -2,33 +2,59 @@ package com.upt.cti.bloodnetwork;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.upt.cti.bloodnetwork.persistence.domain.dto.UserDTO;
+import com.upt.cti.bloodnetwork.serviceHandlers.ServiceCaller;
 
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 public class SignIn extends AppCompatActivity {
 
+    private ServiceCaller serviceCaller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
+        serviceCaller = new ServiceCaller();
     }
 
     public void clicked(View view) {
-        Intent intent;
+        final Intent intent;
         switch (view.getId()) {
             case R.id.signup:
                 intent = new Intent(this, SignUp.class);
                 startActivity(intent);
                 break;
             case R.id.btn_login:
+                final String email = (((EditText)findViewById(R.id.input_email)).getText().toString());
                 intent = new Intent(this, Menu.class);
-                startActivity(intent);
+                Thread thread = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try  {
+                            String url = serviceCaller.host+"/find/"+email;
+                            Looper.prepare();
+                            RestTemplate restTemplate = new RestTemplate();
+                            restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+                            UserDTO result = restTemplate.getForObject(url, UserDTO.class);
+                            if(result != null){
+                                startActivity(intent);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.start();
                 break;
         }
     }
